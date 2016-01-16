77c3e94c4cbc9e05b2ec9ac661f63bad1fd8c3ff
MrGFHCI <scott.grondin@gmail.com>
2016-01-16 11:44:55 -0500

    Same as 1 but in separate class files.

---
+++ Scene2d2/desktop/src/g/scratch/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package g.scratch.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import g.scratch.Scene2D2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Scene2D2(), config);
+	}
+}
